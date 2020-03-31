be77dffbbe872d7a0262d9c377b4ce24445458d0
Timur <timur.bazarov@phystech.edu>
2020-03-31 17:56:52 +0300

    1) Initial MI = 0.85, initial alpha = -0.65.
    2) OSNRreq vs dispersion from ER and Jitter added.

--- SensitivityCalc.m
+++ SensitivityCalc.m
@@ -1,26 +1,45 @@
 clear;close all;
 
-data=readtable('ERandJitter_vs_Disp.xlsx');
+data=readtable('ERandJitter_vs_Disp_2.xlsx');
 fSize=40;
-%plot(data.Disp, data.ER)
 
 QfactorReq=7;
-NTIA = 1;
-Rval=0.5;
-OMAmin=2*NTIA*QfactorReq/Rval;
-Sens=OMAmin/2*(data.ER+1)./(data.ER-1);
+L=80;
+d=18;
 
-%plot(data.Disp,10*log10(Sens/1000))
+%time is measured in unit intervals (UI)
+BW=7;%ГГц
+tb=1/10;
+tr=0.22/BW;
+tr=tr/tb;
+JTpp=0.3;
+DJpp=data.RMSJitter*0;
 
 OSNRreq=2*QfactorReq*(data.ER+1)./(data.ER-1);
 OSNRreq=10*log10(OSNRreq);
-plot(data.Disp+18*80,OSNRreq,'Linewidth',5);
-%xlabel('Остаточная дисперсия');
-%ylabel('Требуемый OSNR')
+figure;
+plot(data.Disp+L*d,OSNRreq,'Linewidth',5);
 
+ylim([12,16])
 set(gca,'FontSize',fSize-4)
 xlabel('Remaining dispersion, ps/nm','Interpreter','latex','FontSize', fSize, 'Color', 'black', 'FontWeight', 'bold');
 ylabel('OSNRreq, dB','Interpreter','latex','FontSize', fSize, 'Color', 'black', 'FontWeight', 'bold');
 
+OSNRreqJ=2*QfactorReq*tr./((JTpp-DJpp)*0.6);
+OSNRreqJ=10*log10(OSNRreqJ);
 
-%plot(data.Disp,data.Q)
+figure;
+plot(data.Disp+L*d,OSNRreqJ,'Linewidth',5);
+
+set(gca,'FontSize',fSize-4)
+xlabel('Remaining dispersion, ps/nm','Interpreter','latex','FontSize', fSize, 'Color', 'black', 'FontWeight', 'bold');
+ylabel('OSNRreqJit, dB','Interpreter','latex','FontSize', fSize, 'Color', 'black', 'FontWeight', 'bold');
+ylim([12,16])
+
+figure;
+plot(data.Disp+L*d,max(OSNRreq,OSNRreqJ),'Linewidth',5);
+
+set(gca,'FontSize',fSize-4)
+xlabel('Remaining dispersion, ps/nm','Interpreter','latex','FontSize', fSize, 'Color', 'black', 'FontWeight', 'bold');
+ylabel('OSNRreqJit, dB','Interpreter','latex','FontSize', fSize, 'Color', 'black', 'FontWeight', 'bold');
+ylim([12,16])
